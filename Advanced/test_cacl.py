2a789084302aec194527a4d81f33fd34c3ac11f0
13259727865 <13259727865@163.com>
2021-02-03 13:59:11 +0800

    pytest第一次作业

--- Advanced/test_cacl.py
+++ Advanced/test_cacl.py
@@ -29,7 +29,7 @@ class Test_cacl:
 	def test_add_2(self,a,b,c):
 		sum = self.cacl1.add(b,a)
 		assert sum == c
-
+	#除法用例方法
 	@pytest.mark.parametrize('a,b,c', data['div'], ids = data['divid'])
 	def test_div(self,a,b,c):
 		sum = self.cacl1.div(a,b)
